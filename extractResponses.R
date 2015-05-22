9b3ef3be97ba4e89d3ccd8713387ccb151a95129
Dustin Hodge <dlhodge@uw.edu>
2015-05-22 10:40:41 -0700

    function header
    Documented what the script/function is supposed to do

--- extractResponses.R
+++ extractResponses.R
@@ -1,2 +1,10 @@
-
-
+#
+# Extract responses will take a csv file (from a Google Sheet), representing
+# survey responses, and split each response into an individual, standalone file
+#
+# @dir - The directory the files should be placed in
+# @csv - The data to read from
+#
+extractResponses <- function(dir, csv) {
+    
+}
