9a959b6a482f25b068ffc875e97406002e52be38
Dustin Hodge <dlhodge@uw.edu>
2015-05-22 10:48:20 -0700

    Reading + transposing
    Got the part where we read the data in and make it row-wise so we can
    access it via individual entries

--- extractResponses.R
+++ extractResponses.R
@@ -2,9 +2,19 @@
 # Extract responses will take a csv file (from a Google Sheet), representing
 # survey responses, and split each response into an individual, standalone file
 #
+# Put together with much help from stack exchage
+# http://stackoverflow.com/questions/30248776/how-to-sequentially-go-through-every-row-and-then-every-column-in-an-r-data-fram
+#
 # @dir - The directory the files should be placed in
 # @csv - The data to read from
 #
 extractResponses <- function(dir, csv) {
+    
+    # reading the data in as a matrix so we can make the data row-wise
+    data <- as.matrix(read.csv(csv))
+    
+    # makes use of recycling in R to match up the column names (the questions) with
+    # each row of responses
+    dataByRow <- paste(colnames(qdata.m), "-------", t(qdata.m), sep = "\n")
     
 }
